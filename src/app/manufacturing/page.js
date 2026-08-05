068f037827338bb51bf8c35fe8ca82ec43e7dd03
Amitabh Parothia <jandrotia.madhavi@gmail.com>
2026-08-05 22:04:22 +0530

    Removed section

--- src/app/manufacturing/page.js
+++ src/app/manufacturing/page.js
@@ -12,7 +12,7 @@ const ManufacturingPage = () => {
         <ManufacturingSystemSection />
         <QualityCertificationSection />
         <CertificationWall />
-        <ClosingCTA />
+        {/* <ClosingCTA /> */}
         <Footer />
         </>
     )
